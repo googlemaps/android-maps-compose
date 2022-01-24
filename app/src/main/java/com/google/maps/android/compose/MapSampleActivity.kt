a919fd1763d488e3638e1b41866a277f1f3485e9
Chris Arriola <chrisarriola@google.com>
2022-01-24 13:48:39 -0800

    chore: Use Singapore instead of SF in sample.
    Change-Id: Ifb7f78387cf540b2bb229359567e6e722d9e5202

--- app/src/main/java/com/google/maps/android/compose/MapSampleActivity.kt
+++ app/src/main/java/com/google/maps/android/compose/MapSampleActivity.kt
@@ -39,11 +39,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.MapsInitializer
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.PointOfInterest
@@ -55,13 +52,12 @@ class MapSampleActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MapsInitializer.initialize(this)
         setContent {
-            val sanFrancisco = LatLng(37.76, -122.47)
+            val singapore = LatLng(1.35, 103.87)
 
             // Observing and controlling the camera's state can be done with a CameraPositionState
             val cameraPositionState = rememberCameraPositionState {
-                position = CameraPosition.fromLatLngZoom(sanFrancisco, 10f)
+                position = CameraPosition.fromLatLngZoom(singapore, 11f)
             }
 
             var mapType by remember { mutableStateOf(MapType.NORMAL) }
@@ -83,7 +79,7 @@ class MapSampleActivity : ComponentActivity() {
                 ) {
                     // Drawing on the map is accomplished with a child-based API
                     Marker(
-                        position = sanFrancisco,
+                        position = singapore,
                         title = "Zoom in has been tapped $ticker times.",
                         onClick = {
                             Log.d(TAG, "${it.title} was clicked")
@@ -91,7 +87,7 @@ class MapSampleActivity : ComponentActivity() {
                         }
                     )
                     Circle(
-                        center = sanFrancisco,
+                        center = singapore,
                         fillColor = MaterialTheme.colors.secondary,
                         strokeColor = MaterialTheme.colors.secondaryVariant,
                         radius = 1000.0,
