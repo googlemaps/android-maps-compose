a327ee2c607db762ba59aabfa43821ce619c4dcc
Chris Arriola <c.rriola@gmail.com>
2022-03-04 19:51:36 -0800

    chore(Test): Add UI tests (#37)
    * chore(Test): Add androidTest.
    
    Change-Id: I35a30c7b70bbb88804db8ce5e0e8146c4aa5cb53
    
    * Add connectedCheck step.
    
    Change-Id: I0a1530cc9133f2d07f682917f7ac709aee3e8406
    
    * Run connectedCheck on app only
    
    Change-Id: I58d97e288db98eac56ef6df9659b653ac2aa92b2
    
    * Upload test reports.
    
    Change-Id: I41ec0a6b30d77bf9803e53e7d6f93efbf01e642b
    
    * Remove failing test.
    
    Change-Id: I14c1b96d13fe796c5145de79df3f1f25046f00c6
    
    * Add timeout to await call.
    
    Change-Id: I7dfd70c1e15f7a9c357d506006aae3c3c3426636
    
    * Inject maps API key.
    
    Change-Id: Ia3fdd45c839f64df9610b93cc0023c80ab72e465
    
    * Injecting api key sooner.
    
    Change-Id: I106362be7128ebdeed133febfc1c8dc2ad062370
    
    * Create local.properties file.
    
    Change-Id: Ifab425e1a664da74c407bb6d2bf7bf76d1cd3854
    
    * Move api key inject a step before connectedCheck.
    
    Change-Id: Idf737d9d1e7eab3d9b94d60d11cd9f2b0d8a70b2
    
    * Simplify test workflow.
    
    Change-Id: Ic55a817da8fb344ac0185c50c107e582b6f42de5
    
    * Add back synced test.yml and rename to instrumentation-test.yml
    
    Change-Id: I3a9ffe7b48591e7e0ec0c79f79d86a34aec6110a
    
    * Add debug statement.
    
    Change-Id: Ia75de3fe64826dea3cbd807a1c82cbd06e06a291
    
    * Inject API key directly in AndroidManifest.xml
    
    Change-Id: Ie922328946106712d66791a0589f592b3109b36a
    
    * Update layout on sample app.
    
    Change-Id: I26075247a5cf0ba5a889fb67fbb08b6312cd076a
    
    * Use a single API level.
    
    Change-Id: Ifd2aa8abfce30251207e057565b6694787164db6
    
    * s/waitForIdle/waitUntil
    
    Change-Id: Icef7d759573d1459a37e3d9058a1214ee2e52301
    
    * Adjust rounding error.
    
    Change-Id: Ieefa005ab8c83e3d287a53f00c8b79c229f5a372

--- app/src/main/java/com/google/maps/android/compose/MapSampleActivity.kt
+++ app/src/main/java/com/google/maps/android/compose/MapSampleActivity.kt
@@ -46,6 +46,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMapOptions
@@ -57,19 +58,27 @@ import kotlinx.coroutines.launch
 
 private const val TAG = "MapSampleActivity"
 
+val singapore = LatLng(1.35, 103.87)
+val singapore2 = LatLng(1.40, 103.77)
+
 class MapSampleActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             var isMapLoaded by remember { mutableStateOf(false) }
+            // Observing and controlling the camera's state can be done with a CameraPositionState
+            val cameraPositionState = rememberCameraPositionState {
+                position = CameraPosition.fromLatLngZoom(singapore, 11f)
+            }
 
             Box(Modifier.fillMaxSize()) {
                 GoogleMapView(
                     modifier = Modifier.matchParentSize(),
+                    cameraPositionState = cameraPositionState,
                     onMapLoaded = {
                         isMapLoaded = true
-                    }
+                    },
                 )
                 if (!isMapLoaded) {
                     AnimatedVisibility(
@@ -92,25 +101,17 @@ class MapSampleActivity : ComponentActivity() {
 }
 
 @Composable
-private fun GoogleMapView(modifier: Modifier, onMapLoaded: () -> Unit) {
-    val singapore = LatLng(1.35, 103.87)
-    val singapore2 = LatLng(1.40, 103.77)
-
-    // Observing and controlling the camera's state can be done with a CameraPositionState
-    val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 11f)
-    }
-
+fun GoogleMapView(
+    modifier: Modifier,
+    cameraPositionState: CameraPositionState,
+    onMapLoaded: () -> Unit,
+) {
+    var uiSettings by remember { mutableStateOf(MapUiSettings(compassEnabled = false)) }
+    var shouldAnimateZoom by remember { mutableStateOf(true) }
+    var ticker by remember { mutableStateOf(0) }
     var mapProperties by remember {
         mutableStateOf(MapProperties(mapType = MapType.NORMAL))
     }
-    var uiSettings by remember {
-        mutableStateOf(
-            MapUiSettings(compassEnabled = false)
-        )
-    }
-    var shouldAnimateZoom by remember { mutableStateOf(true) }
-    var ticker by remember { mutableStateOf(0) }
 
     GoogleMap(
         modifier = modifier,
@@ -232,20 +233,17 @@ private fun ZoomControls(
         MapButton("-", onClick = { onZoomOut() })
         MapButton("+", onClick = { onZoomIn() })
         Column(verticalArrangement = Arrangement.Center) {
-            Row(horizontalArrangement = Arrangement.Center) {
-                Text(text = "Camera Animations On?")
-                Switch(
-                    isCameraAnimationChecked,
-                    onCheckedChange = onCameraAnimationCheckedChange
-                )
-            }
-            Row(horizontalArrangement = Arrangement.Center) {
-                Text(text = "Zoom Controls On?")
-                Switch(
-                    isZoomControlsEnabledChecked,
-                    onCheckedChange = onZoomControlsCheckedChange
-                )
-            }
+            Text(text = "Camera Animations On?")
+            Switch(
+                isCameraAnimationChecked,
+                onCheckedChange = onCameraAnimationCheckedChange,
+                modifier = Modifier.testTag("cameraAnimations"),
+            )
+            Text(text = "Zoom Controls On?")
+            Switch(
+                isZoomControlsEnabledChecked,
+                onCheckedChange = onZoomControlsCheckedChange
+            )
         }
     }
 }
