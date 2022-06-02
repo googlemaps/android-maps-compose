2f27a84f79cffb6f5ced95d5a227f599a294f479
Chris Arriola <carriola@google.com>
2022-06-01 21:11:40 -0700

    Address PR feedback.
    Change-Id: I2819da87f7b3c3f1505402ff718b2139b6b1d114

--- maps-compose/src/main/java/com/google/maps/android/compose/GoogleMapComposable.kt
+++ maps-compose/src/main/java/com/google/maps/android/compose/GoogleMapComposable.kt
@@ -5,6 +5,9 @@ import androidx.compose.runtime.ComposableTargetMarker
 /**
  * An annotation that can be used to mark a composable function as being expected to be use in a
  * composable function that is also marked or inferred to be marked as a [GoogleMapComposable].
+ *
+ * This will produce build warnings when [GoogleMapComposable] composable functions are used outside
+ * of a [GoogleMapComposable] content lambda, and vice versa.
  */
 @Retention(AnnotationRetention.BINARY)
 @ComposableTargetMarker(description = "Google Map Composable")
