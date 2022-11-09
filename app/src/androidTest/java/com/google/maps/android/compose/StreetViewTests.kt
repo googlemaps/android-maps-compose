414d86414395e35da2e9b499036743d8468093da
Chris Arriola <carriola@google.com>
2022-11-09 09:19:54 -0800

    Clean tests
    Change-Id: I0c19a5ec5b716b22bffa05f0c916fb67fca745be

--- app/src/androidTest/java/com/google/maps/android/compose/StreetViewTests.kt
+++ app/src/androidTest/java/com/google/maps/android/compose/StreetViewTests.kt
@@ -1,12 +1,15 @@
 package com.google.maps.android.compose
 
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.google.android.gms.maps.StreetViewPanoramaOptions
-import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.StreetViewPanoramaOrientation
 import com.google.maps.android.compose.streetview.StreetView
 import com.google.maps.android.compose.streetview.StreetViewCameraPositionState
 import com.google.maps.android.ktx.MapsExperimentalFeature
-import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -14,34 +17,36 @@ class StreetViewTests {
     @get:Rule
     val composeTestRule = createComposeRule()
 
+    private lateinit var cameraPositionState: StreetViewCameraPositionState
+    private val initialLatLng = singapore
+
+    @Before
+    fun setUp() {
+        cameraPositionState = StreetViewCameraPositionState()
+    }
+
     @OptIn(MapsExperimentalFeature::class)
-    @Test
-    fun testStartingStreetViewPosition() {
-        val cameraPositionState = StreetViewCameraPositionState()
-        val initialLatLng = singapore
+    private fun initStreetView(onClick: (StreetViewPanoramaOrientation) -> Unit = {}) {
         composeTestRule.setContent {
             StreetView(
+                Modifier.semantics { contentDescription = "StreetView" },
                 cameraPositionState = cameraPositionState,
                 streetViewPanoramaOptionsFactory = {
                     StreetViewPanoramaOptions()
                         .position(initialLatLng)
-                }
+                },
+                onClick = onClick
             )
         }
         composeTestRule.waitUntil(8000) {
             cameraPositionState.location.position.latitude != 0.0 &&
                 cameraPositionState.location.position.longitude != 0.0
         }
+    }
+
+    @Test
+    fun testStartingStreetViewPosition() {
+        initStreetView()
         initialLatLng.assertEquals(cameraPositionState.location.position)
-    }
-
-    @Test
-    fun testStreetViewOnClick() {
-        TODO()
-    }
-
-    @Test
-    fun testStreetViewOnLongClick() {
-        TODO()
     }
 }
