e8383bcd17a1393f7e37a0cadadf94edbe45bd07
Steven Schoen <steven.schoen@reddit.com>
2022-12-07 21:00:02 -0500

    Make MarkerState stable

--- maps-compose/src/main/java/com/google/maps/android/compose/Marker.kt
+++ maps-compose/src/main/java/com/google/maps/android/compose/Marker.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ComposeNode
 import androidx.compose.runtime.CompositionContext
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.currentComposer
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -81,13 +82,15 @@ public class MarkerState(
         internal set
 
     // The marker associated with this MarkerState.
-    internal var marker: Marker? = null
+    private val markerState: MutableState<Marker?> = mutableStateOf(null)
+    internal var marker: Marker?
+        get() = markerState.value
         set(value) {
-            if (field == null && value == null) return
-            if (field != null && value != null) {
+            if (markerState.value == null && value == null) return
+            if (markerState.value != null && value != null) {
                 error("MarkerState may only be associated with one Marker at a time.")
             }
-            field = value
+            markerState.value = value
         }
 
     /**
