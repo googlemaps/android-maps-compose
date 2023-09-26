ac08b6990a1a90402d5b96a9daea9ae4494cc203
Enrique López-Manas <eenriquelopez@gmail.com>
2023-09-26 16:59:44 +0200

    chore: updated more libraries

--- maps-compose/src/main/java/com/google/maps/android/compose/MapApplier.kt
+++ maps-compose/src/main/java/com/google/maps/android/compose/MapApplier.kt
@@ -1,4 +1,4 @@
-// Copyright 2021 Google LLC
+// Copyright 2023 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -110,7 +110,6 @@ internal class MapApplier(
                 nodeInputCallback = { onMarkerClick },
                 inputHandlerCallback = { onMarkerClick }
             )
-                ?: false
         }
         map.setOnInfoWindowClickListener { marker ->
             decorations.findInputCallback<MarkerNode, Marker, Unit>(
