fcdc8336f114a49704cfc5d08f2f7e26bc7a501d
Philip <psegerfast@gmail.com>
2024-02-22 13:10:26 +0100

    Simplify MapViewLifecycleController logic

--- maps-compose/src/main/java/com/google/maps/android/compose/MapViewLifecycleController.kt
+++ maps-compose/src/main/java/com/google/maps/android/compose/MapViewLifecycleController.kt
@@ -3,8 +3,10 @@ package com.google.maps.android.compose
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.Lifecycle.Event
 import androidx.lifecycle.LifecycleEventObserver
 import com.google.android.gms.maps.MapView
+import com.google.maps.android.compose.MapViewLifecycleController.LifecycleDirection.*
 
 internal class MapViewLifecycleController(
     isMapReused: Boolean,
@@ -16,45 +18,20 @@ internal class MapViewLifecycleController(
     }
 
     private companion object {
-        /**
-         * Used to navigate up/down through lifecycle state.
-         * [up] and [down] are nullable suppliers to avoid circular references.
-         *
-         * Up means if a lifecycle goes "upwards", ie onCreate -> onResume.
-         * Down means the opposite, ie onResume -> onDestroy.
-         *
-         * [developer.android.com/...](https://developer.android.com/guide/components/activities/activity-lifecycle)
-         * */
-        sealed class LifecycleStateNavigation(
-            val lifecycle: Lifecycle.Event,
-            val up: (() -> LifecycleStateNavigation)?,
-            val down: (() -> LifecycleStateNavigation)?
-        ) {
-            companion object {
-                private val instances = listOf(OnDestroy, OnStop, OnPause, OnCreate, OnStart, OnResume)
-                fun forLifecycleEvent(lifecycleEvent: Lifecycle.Event) = instances.first { it.lifecycle == lifecycleEvent }
-            }
-        }
-
         val lifecycleUp = listOf(
-            Lifecycle.Event.ON_CREATE,
-            Lifecycle.Event.ON_START,
-            Lifecycle.Event.ON_RESUME
+            Event.ON_CREATE,
+            Event.ON_START,
+            Event.ON_RESUME
         )
-
-        data object OnDestroy: LifecycleStateNavigation(Lifecycle.Event.ON_DESTROY, null, null)
-        data object OnStop: LifecycleStateNavigation(Lifecycle.Event.ON_STOP, { OnStart }, { OnDestroy })
-        data object OnPause: LifecycleStateNavigation(Lifecycle.Event.ON_PAUSE, { OnResume }, { OnStop })
-        data object OnCreate: LifecycleStateNavigation(Lifecycle.Event.ON_CREATE, { OnStart }, null)
-        data object OnStart: LifecycleStateNavigation(Lifecycle.Event.ON_START, { OnResume }, null)
-        data object OnResume: LifecycleStateNavigation(Lifecycle.Event.ON_RESUME, null, { OnPause })
     }
 
-    private var created = isMapReused
+    private enum class LifecycleDirection {
+        Up, Down
+    }
 
     private var previousLifecycleState = if (isMapReused)
-        Lifecycle.Event.ON_STOP else
-        Lifecycle.Event.ON_CREATE
+        Event.ON_STOP else
+        Event.ON_CREATE
 
     var lifecycle: Lifecycle? = null
         set(value) {
@@ -67,64 +44,76 @@ internal class MapViewLifecycleController(
 
     fun onDestroy() {
         lifecycle = null
-        moveToLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+        moveToLifecycleEvent(Event.ON_DESTROY)
     }
 
     fun onLifecycleDetached() {
         lifecycle = null
-        moveToLifecycleEvent(Lifecycle.Event.ON_STOP)
+        moveToLifecycleEvent(Event.ON_STOP)
     }
 
     /**
      * Move to the lifecycle event instead of setting it directly.
-     *
-     *
      * */
-    private fun moveToLifecycleEvent(targetLifecycle: Lifecycle.Event) {
+    private fun moveToLifecycleEvent(targetLifecycle: Event) {
         if(targetLifecycle == previousLifecycleState) return
 
-        val moveUp = targetLifecycle in lifecycleUp
-        val strDirection = if(moveUp) "UP" else "DOWN"
-        var currentLifecycleNavigator = LifecycleStateNavigation.forLifecycleEvent(previousLifecycleState)
-        Log.d(lcTag, "Moving $strDirection from ${currentLifecycleNavigator.lifecycle} to $targetLifecycle.")
+        val lifecycleDirection = if(targetLifecycle in lifecycleUp) Up else Down
 
-        while(currentLifecycleNavigator.lifecycle != targetLifecycle) {
-            buildString {
-                appendLine("==========================================")
-                appendLine("Current navigator: ${currentLifecycleNavigator.lifecycle}")
-                appendLine("up: ${currentLifecycleNavigator.up?.invoke()?.lifecycle}")
-                appendLine("down: ${currentLifecycleNavigator.down?.invoke()?.lifecycle}")
-                appendLine("Moving: $strDirection")
-                appendLine("------------------------------------------")
-            }.also {
-                Log.d(lcTag, it)
-            }
-            currentLifecycleNavigator = if(moveUp)
-                currentLifecycleNavigator.up!!.invoke() else
-                currentLifecycleNavigator.down!!.invoke()
-
-            setLifecycleEvent(currentLifecycleNavigator.lifecycle)
+        (if(lifecycleDirection == Up) "UP" else "DOWN").let { strDirection ->
+            Log.d(lcTag, "Moving $strDirection from $previousLifecycleState to $targetLifecycle.")
         }
+
+        do {
+            val nextLifecycleState = nextLifecycleEvent(lifecycleDirection)
+            setLifecycleEvent(nextLifecycleState)
+        } while(nextLifecycleState != targetLifecycle)
     }
 
-    private fun setLifecycleEvent(event: Lifecycle.Event) {
+    private fun nextLifecycleEvent(direction: LifecycleDirection) = when(previousLifecycleState) {
+        Event.ON_CREATE -> when(direction) {
+            Up -> Event.ON_START
+            Down -> error("No lifecycle event below ON_CREATE.")
+        }
+        Event.ON_START -> when(direction) {
+            Up -> Event.ON_RESUME
+            Down -> error("No lifecycle event below ON_START.")
+        }
+        Event.ON_RESUME -> when(direction) {
+            Up -> error("No lifecycle event above ON_RESUME.")
+            Down -> Event.ON_PAUSE
+        }
+        Event.ON_PAUSE -> when(direction) {
+            Up -> Event.ON_RESUME
+            Down -> Event.ON_STOP
+        }
+        Event.ON_STOP -> when(direction) {
+            Up -> Event.ON_START
+            Down -> Event.ON_DESTROY
+        }
+        Event.ON_DESTROY -> error("No lifecycle event above ON_DESTROY")
+        Event.ON_ANY -> error("Unsupported operation")
+    }
+
+
+    private fun setLifecycleEvent(event: Event) {
         Log.d(lcTag, "Invoking: $event!")
 
         when (event) {
-            Lifecycle.Event.ON_CREATE -> {
+            Event.ON_CREATE -> {
                 // Skip calling mapView.onCreate if the lifecycle did not go through onDestroy - in
                 // this case the GoogleMap composable also doesn't leave the composition. So,
                 // recreating the map does not restore state properly which must be avoided.
-                if (previousLifecycleState != Lifecycle.Event.ON_STOP) {
+                if (previousLifecycleState != Event.ON_STOP) {
                     mapView.onCreate(Bundle())
                 }
             }
 
-            Lifecycle.Event.ON_START -> mapView.onStart()
-            Lifecycle.Event.ON_RESUME -> mapView.onResume()
-            Lifecycle.Event.ON_PAUSE -> mapView.onPause()
-            Lifecycle.Event.ON_STOP -> mapView.onStop()
-            Lifecycle.Event.ON_DESTROY -> {
+            Event.ON_START -> mapView.onStart()
+            Event.ON_RESUME -> mapView.onResume()
+            Event.ON_PAUSE -> mapView.onPause()
+            Event.ON_STOP -> mapView.onStop()
+            Event.ON_DESTROY -> {
                 // Handled in AndroidView onRelease
             }
 
@@ -133,18 +122,20 @@ internal class MapViewLifecycleController(
         previousLifecycleState = event
     }
 
+    private var created = isMapReused
+
     private val observer = LifecycleEventObserver { _, event ->
         Log.d(lcTag, "---===[ LEO: Lifecycle event received from LifecycleEventObserver: $event. ]===---")
 
         if(!created) {
             Log.d(lcTag, "LEO: Invoking initial ON_CREATE.")
             created = true
-            setLifecycleEvent(Lifecycle.Event.ON_CREATE)
-        } else if(event == Lifecycle.Event.ON_CREATE) {
+            setLifecycleEvent(Event.ON_CREATE)
+        } else if(event == Event.ON_CREATE) {
             Log.d(lcTag, "LEO: ON_CREATE lifecycle event was received but view is already created.")
         }
 
-        if(event != Lifecycle.Event.ON_CREATE) {
+        if(event != Event.ON_CREATE) {
             moveToLifecycleEvent(event)
         }
     }
