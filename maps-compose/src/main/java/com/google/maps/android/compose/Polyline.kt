ad626a7cfd30e3cf93c53fa316457ab5b743dbd7
el-qq <codeisqq@gmail.com>
2024-04-10 14:33:45 +0500

    feat: Add StyleSpans support for Polylines

--- maps-compose/src/main/java/com/google/maps/android/compose/Polyline.kt
+++ maps-compose/src/main/java/com/google/maps/android/compose/Polyline.kt
@@ -25,6 +25,7 @@ import com.google.android.gms.maps.model.JointType
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.PatternItem
 import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.StyleSpan
 import com.google.maps.android.ktx.addPolyline
 
 internal class PolylineNode(
@@ -70,11 +71,116 @@ public fun Polyline(
     zIndex: Float = 0f,
     onClick: (Polyline) -> Unit = {}
 ) {
+    PolylineImpl(
+        points = points,
+        clickable = clickable,
+        color = color,
+        endCap = endCap,
+        geodesic = geodesic,
+        jointType = jointType,
+        pattern = pattern,
+        startCap = startCap,
+        tag = tag,
+        visible = visible,
+        width = width,
+        zIndex = zIndex,
+        onClick = onClick,
+    )
+}
+
+/**
+ * A composable for a polyline on the map.
+ *
+ * @param points the points comprising the polyline
+ * @param spans style spans for the polyline
+ * @param clickable boolean indicating if the polyline is clickable or not
+ * @param endCap a cap at the end vertex of the polyline
+ * @param geodesic specifies whether to draw the polyline as a geodesic
+ * @param jointType the joint type for all vertices of the polyline except the start and end
+ * vertices
+ * @param pattern the pattern for the polyline
+ * @param startCap the cap at the start vertex of the polyline
+ * @param visible the visibility of the polyline
+ * @param width the width of the polyline in screen pixels
+ * @param zIndex the z-index of the polyline
+ * @param onClick a lambda invoked when the polyline is clicked
+ */
+@Composable
+@GoogleMapComposable
+public fun Polyline(
+    points: List<LatLng>,
+    spans: List<StyleSpan>,
+    clickable: Boolean = false,
+    endCap: Cap = ButtCap(),
+    geodesic: Boolean = false,
+    jointType: Int = JointType.DEFAULT,
+    pattern: List<PatternItem>? = null,
+    startCap: Cap = ButtCap(),
+    tag: Any? = null,
+    visible: Boolean = true,
+    width: Float = 10f,
+    zIndex: Float = 0f,
+    onClick: (Polyline) -> Unit = {},
+) {
+    PolylineImpl(
+        points = points,
+        spans = spans,
+        clickable = clickable,
+        endCap = endCap,
+        geodesic = geodesic,
+        jointType = jointType,
+        pattern = pattern,
+        startCap = startCap,
+        tag = tag,
+        visible = visible,
+        width = width,
+        zIndex = zIndex,
+        onClick = onClick,
+    )
+}
+
+/**
+ * Internal implementation for an advanced polyline on a Google map.
+ *
+ * @param points the points comprising the polyline
+ * @param spans style spans for the polyline
+ * @param clickable boolean indicating if the polyline is clickable or not
+ * @param color the color of the polyline
+ * @param endCap a cap at the end vertex of the polyline
+ * @param geodesic specifies whether to draw the polyline as a geodesic
+ * @param jointType the joint type for all vertices of the polyline except the start and end
+ * vertices
+ * @param pattern the pattern for the polyline
+ * @param startCap the cap at the start vertex of the polyline
+ * @param visible the visibility of the polyline
+ * @param width the width of the polyline in screen pixels
+ * @param zIndex the z-index of the polyline
+ * @param onClick a lambda invoked when the polyline is clicked
+ */
+@Composable
+@GoogleMapComposable
+private fun PolylineImpl(
+    points: List<LatLng>,
+    spans: List<StyleSpan> = emptyList(),
+    clickable: Boolean = false,
+    color: Color = Color.Black,
+    endCap: Cap = ButtCap(),
+    geodesic: Boolean = false,
+    jointType: Int = JointType.DEFAULT,
+    pattern: List<PatternItem>? = null,
+    startCap: Cap = ButtCap(),
+    tag: Any? = null,
+    visible: Boolean = true,
+    width: Float = 10f,
+    zIndex: Float = 0f,
+    onClick: (Polyline) -> Unit = {},
+) {
     val mapApplier = currentComposer.applier as MapApplier?
     ComposeNode<PolylineNode, MapApplier>(
         factory = {
             val polyline = mapApplier?.map?.addPolyline {
                 addAll(points)
+                addAllSpans(spans)
                 clickable(clickable)
                 color(color.toArgb())
                 endCap(endCap)
@@ -93,6 +199,7 @@ public fun Polyline(
             update(onClick) { this.onPolylineClick = it }
 
             set(points) { this.polyline.points = it }
+            set(spans) { this.polyline.spans = it }
             set(clickable) { this.polyline.isClickable = it }
             set(color) { this.polyline.color = it.toArgb() }
             set(endCap) { this.polyline.endCap = it }
