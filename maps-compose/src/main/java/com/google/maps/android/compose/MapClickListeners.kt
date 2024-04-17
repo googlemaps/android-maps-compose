13247ce7501e8100ec270458a2f0ed4c1592f205
Philip S <psegerfast@gmail.com>
2024-04-17 12:03:45 +0200

    Undo unrelated refactoring

--- maps-compose/src/main/java/com/google/maps/android/compose/MapClickListeners.kt
+++ maps-compose/src/main/java/com/google/maps/android/compose/MapClickListeners.kt
@@ -88,8 +88,7 @@ internal class MapClickListenerNode<L : Any>(
 @Composable
 internal fun MapClickListenerUpdater() {
     // The mapClickListeners container object is not allowed to ever change
-    val applier = (currentComposer.applier as MapApplier)
-    val mapClickListeners = applier.mapClickListeners
+    val mapClickListeners = (currentComposer.applier as MapApplier).mapClickListeners
 
     with(mapClickListeners) {
         ::indoorStateChangeListener.let { callback ->
