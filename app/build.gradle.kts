914e7d9da308e61aeba52dc6055dc66ed51218a5
Enrique López Mañas <eenriquelopez@gmail.com>
2024-11-05 18:21:15 +0100

    chore: dependencies updated (#645)
    * chore: dependencies updated
    
    * chore: targetSdk upgraded
    
    * chore: upgraded gradle/actions/wrapper-validation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,11 +22,11 @@ android {
     }
 
     namespace = "com.google.maps.android.compose"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
