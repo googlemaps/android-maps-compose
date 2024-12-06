973c397957a6a9fa7846df2e7bce1dee7b07ccec
Enrique López Mañas <eenriquelopez@gmail.com>
2024-12-06 18:14:08 +0100

    chore: added threshold

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,12 @@ android {
     }
 
     experimentalProperties["android.experimental.enableScreenshotTest"] = true
+
+    testOptions {
+        screenshotTests {
+            imageDifferenceThreshold = 0.0001f // 0.01%
+        }
+    }
 }
 
 dependencies {
