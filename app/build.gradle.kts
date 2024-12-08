fe8f007c0bf37694364afa029525234f3bdafe60
Enrique López Mañas <eenriquelopez@gmail.com>
2024-12-08 14:46:27 +0100

    chore: changed threshold to 5%

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 
     testOptions {
         screenshotTests {
-            imageDifferenceThreshold = 0.01f // 1%
+            imageDifferenceThreshold = 0.05f // 1%
         }
     }
 }
