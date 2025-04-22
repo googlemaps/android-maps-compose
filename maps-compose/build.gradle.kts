0c3711b64ea8ac5c60bd2d1491e380ae372066c2
Enrique López Mañas <eenriquelopez@gmail.com>
2025-04-22 19:49:33 +0200

    feat: added Attributionid (#702)
    Co-authored-by: Dale Hawkins <107309+dkhawk@users.noreply.github.com>

--- maps-compose/build.gradle.kts
+++ maps-compose/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.sourceSets
+
 plugins {
     id("org.jetbrains.kotlin.android")
     alias(libs.plugins.compose.compiler)
@@ -33,6 +35,8 @@ android {
             "-Xopt-in=kotlin.RequiresOptIn",
         )
     }
+
+    sourceSets["main"].java.srcDir("build/generated/source/artifactId")
 }
 
 composeCompiler {
@@ -59,4 +63,33 @@ dependencies {
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.test.espresso)
     androidTestImplementation(libs.androidx.test.junit.ktx)
+}
+
+// Artifact ID logic
+val attributionId = "gmp_git_androidmapscompose_v$version"
+
+val generateArtifactIdFile = tasks.register("generateArtifactIdFile") {
+    val outputDir = layout.buildDirectory.dir("generated/source/artifactId")
+    val packageName = "com.google.maps.android.compose.meta"
+    val packagePath = packageName.replace('.', '/')
+    val outputFile = outputDir.get().file("$packagePath/ArtifactId.kt").asFile
+
+    outputs.file(outputFile)
+
+    doLast {
+        outputFile.parentFile.mkdirs()
+        outputFile.writeText(
+            """
+            package $packageName
+
+            public object AttributionId {
+                public const val VALUE: String = "$attributionId"
+            }
+            """.trimIndent()
+        )
+    }
+}
+
+tasks.named("preBuild") {
+    dependsOn(generateArtifactIdFile)
 }
