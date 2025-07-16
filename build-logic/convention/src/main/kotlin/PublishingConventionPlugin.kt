96fa5088f5baa53da88e89a0111a630f5596c217
Enrique López Mañas <eenriquelopez@gmail.com>
2025-07-16 12:25:29 +0200

    chore: migrated to vanniktech Maven plugin

--- build-logic/convention/src/main/kotlin/PublishingConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/PublishingConventionPlugin.kt
@@ -1,14 +1,13 @@
 // buildSrc/src/main/kotlin/PublishingConventionPlugin.kt
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.MavenPublishBaseExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.publish.PublishingExtension
-import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.kotlin.dsl.*
 import org.gradle.testing.jacoco.plugins.JacocoPluginExtension
 import org.gradle.api.tasks.testing.Test
 import org.gradle.testing.jacoco.plugins.JacocoTaskExtension
-import org.gradle.plugins.signing.SigningExtension
-import org.gradle.api.publish.maven.*
 
 class PublishingConventionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
@@ -16,17 +15,15 @@ class PublishingConventionPlugin : Plugin<Project> {
 
             applyPlugins()
             configureJacoco()
-            configurePublishing()
-            configureSigning()
+            configureVanniktechPublishing()
         }
     }
 
     private fun Project.applyPlugins() {
         apply(plugin = "com.android.library")
         apply(plugin = "com.mxalbert.gradle.jacoco-android")
-        apply(plugin = "maven-publish")
         apply(plugin = "org.jetbrains.dokka")
-        apply(plugin = "signing")
+        apply(plugin = "com.vanniktech.maven.publish")
     }
 
     private fun Project.configureJacoco() {
@@ -43,68 +40,42 @@ class PublishingConventionPlugin : Plugin<Project> {
         }
     }
 
-    private fun Project.configurePublishing() {
-        extensions.configure<com.android.build.gradle.LibraryExtension> {
-            publishing {
-                singleVariant("release") {
-                    withSourcesJar()
-                    withJavadocJar()
-                }
-            }
-        }
-        extensions.configure<PublishingExtension> {
-            publications {
-                create<MavenPublication>("aar") {
-                    afterEvaluate {
-                        from(components["release"])
-                    }
-                    pom {
-                        name.set(project.name)
-                        description.set("Jetpack Compose components for the Maps SDK for Android")
-                        url.set("https://github.com/googlemaps/android-maps-compose")
-                        scm {
-                            connection.set("scm:git@github.com:googlemaps/android-maps-compose.git")
-                            developerConnection.set("scm:git@github.com:googlemaps/android-maps-compose.git")
-                            url.set("https://github.com/googlemaps/android-maps-compose")
-                        }
-                        licenses {
-                            license {
-                                name.set("The Apache Software License, Version 2.0")
-                                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                                distribution.set("repo")
-                            }
-                        }
-                        organization {
-                            name.set("Google Inc")
-                            url.set("http://developers.google.com/maps")
-                        }
-                        developers {
-                            developer {
-                                name.set("Google Inc.")
-                            }
-                        }
-                    }
-                }
-            }
-            repositories {
-                maven {
-                    val releasesRepoUrl =
-                        uri("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/")
-                    val snapshotsRepoUrl =
-                        uri("https://central.sonatype.com/repository/maven-snapshots/")
-                    url = if (project.version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-                    credentials {
-                        username = project.findProperty("sonatypeToken") as String?
-                        password = project.findProperty("sonatypeTokenPassword") as String?
-                    }
-                }
-            }
-        }
-    }
+    private fun Project.configureVanniktechPublishing() {
+        extensions.configure<MavenPublishBaseExtension> {
+            configure(
+                AndroidSingleVariantLibrary(
+                    variant = "release",
+                    sourcesJar = true,
+                    publishJavadocJar = true
+                )
+            )
 
-    private fun Project.configureSigning() {
-        configure<SigningExtension> {
-            sign(extensions.getByType<PublishingExtension>().publications["aar"])
+            publishToMavenCentral()
+            signAllPublications()
+
+            pom {
+                name.set(project.name)
+                description.set("Jetpack Compose components for the Maps SDK for Android")
+                url.set("https://github.com/googlemaps/android-maps-compose")
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+                scm {
+                    connection.set("scm:git@github.com:googlemaps/android-maps-compose.git")
+                    developerConnection.set("scm:git@github.com:googlemaps/android-maps-compose.git")
+                    url.set("https://github.com/googlemaps/android-maps-compose")
+                }
+                developers {
+                    developer {
+                        id.set("google")
+                        name.set("Google Inc.")
+                    }
+                }
+            }
         }
     }
 }
