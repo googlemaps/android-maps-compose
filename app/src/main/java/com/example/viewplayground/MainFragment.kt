67dac2658994722396a71f6b4a6c965d78731cef
Enrique López-Mañas <eenriquelopez@gmail.com>
2025-07-21 17:31:21 +0200

    sample: error with width and height

---
+++ app/src/main/java/com/example/viewplayground/MainFragment.kt
@@ -0,0 +1,35 @@
+package com.example.viewplayground
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MarkerComposable
+
+class MainFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = ComposeView(requireContext()).apply {
+        setViewCompositionStrategy(
+            ViewCompositionStrategy.DisposeOnDetachedFromWindow
+        )
+        layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        setContent {
+            GoogleMap {
+                MarkerComposable {
+                    Text(text = "Hello, World!")
+                }
+            }
+        }
+    }
+}
