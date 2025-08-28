d71931e642a78a2248316e381b72dec141263c6f
Enrique López Mañas <eenriquelopez@gmail.com>
2025-08-28 16:54:18 +0200

    build: libraries updated

--- maps-app/build.gradle.kts
+++ maps-app/build.gradle.kts
@@ -23,11 +23,11 @@ android {
     }
 
     namespace = "com.google.maps.android.compose"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
