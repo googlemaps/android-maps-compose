0cd0f49d93e85cc048fef05b8598603e782cb3db
dkhawk <107309+dkhawk@users.noreply.github.com>
2025-09-09 05:00:02 -0600

    test: Add StrictMode check to GoogleMapsInitializerTest
    Wraps the `GoogleMapsInitializer.initialize()` call with strict
    StrictMode policies. This ensures the initialization process does not
    perform any violations, such as disk reads, which would cause the
    test to fail.

--- maps-app/src/androidTest/java/com/google/maps/android/compose/GoogleMapsInitializerTest.kt
+++ maps-app/src/androidTest/java/com/google/maps/android/compose/GoogleMapsInitializerTest.kt
@@ -1,6 +1,7 @@
 package com.google.maps.android.compose
 
 import android.content.Context
+import android.os.StrictMode
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.google.common.truth.Truth.assertThat
@@ -25,9 +26,35 @@ class GoogleMapsInitializerTest {
     fun testInitializationSuccess() = runTest {
         // In an instrumentation test environment, Google Play services are available.
         // Therefore, we expect the initialization to succeed.
+
         val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
 
+        // Note: we need to establish the Strict Mode settings here as there are violations outside
+        // of our control if we try to set them in setUp
+        val threadPolicy = StrictMode.getThreadPolicy()
+        val vmPolicy = StrictMode.getVmPolicy()
+
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectAll()
+                .penaltyLog()
+                .penaltyDeath()
+                .build()
+        )
+        StrictMode.setVmPolicy(
+            StrictMode.VmPolicy.Builder()
+                .detectAll()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build()
+        )
+
         GoogleMapsInitializer.initialize(context)
+
+        StrictMode.setThreadPolicy(threadPolicy)
+        StrictMode.setVmPolicy(vmPolicy)
 
         assertThat(GoogleMapsInitializer.state.value).isEqualTo(InitializationState.SUCCESS)
     }
