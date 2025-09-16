db43b9c03fc6c9a5148ce360a4641d1d65913b79
dkhawk <107309+dkhawk@users.noreply.github.com>
2025-09-16 16:11:06 -0600

    refactor(demo): Extract strings and refactor list composables
    Refactored the demo list screen to support internationalization by extracting all hardcoded strings into `strings.xml`. The `Activity` and `ActivityGroup` data classes were updated to use `@StringRes` integer IDs instead of `String` literals.
    
    Additionally, the `DemoList` composable has been broken down into smaller, more focused composables (`GroupHeaderItem` and `DemoActivityItem`) to improve readability and maintainability.

--- maps-app/src/main/java/com/google/maps/android/compose/Demo.kt
+++ maps-app/src/main/java/com/google/maps/android/compose/Demo.kt
@@ -2,6 +2,7 @@
 package com.google.maps.android.compose
 
 import androidx.activity.ComponentActivity
+import androidx.annotation.StringRes
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -25,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.google.maps.android.compose.markerexamples.AdvancedMarkersActivity
@@ -49,119 +51,119 @@ import kotlin.reflect.KClass
  * @param activities The list of activities belonging to this group.
  */
 sealed class ActivityGroup(
-    val title: String,
+    @StringRes val title: Int,
     val activities: List<Activity>
 ) {
     object MapTypes : ActivityGroup(
-        "Map Types",
+        R.string.map_types_title,
         listOf(
             Activity(
-                "Basic Map",
-                "A simple map showing the default configuration.",
+                R.string.basic_map_activity,
+                R.string.basic_map_activity_description,
                 BasicMapActivity::class
             ),
             Activity(
-                "Street View",
-                "A simple Street View.",
+                R.string.street_view_activity,
+                R.string.street_view_activity_description,
                 StreetViewActivity::class
             ),
         )
     )
 
     object MapFeatures : ActivityGroup(
-        "Map Features",
+        R.string.map_features_title,
         listOf(
             Activity(
-                "Location Tracking",
-                "Tracking your location on the map.",
+                R.string.location_tracking_activity,
+                R.string.location_tracking_activity_description,
                 LocationTrackingActivity::class
             ),
             Activity(
-                "Scale Bar",
-                "Displaying a scale bar on the map.",
+                R.string.scale_bar_activity,
+                R.string.scale_bar_activity_description,
                 ScaleBarActivity::class
             ),
             Activity(
-                "Custom Controls",
-                "Replacing the default location button with a custom one.",
+                R.string.custom_controls_activity,
+                R.string.custom_controls_activity_description,
                 CustomControlsActivity::class
             ),
             Activity(
-                "Accessibility",
-                "Making your map more accessible.",
+                R.string.accessibility_activity,
+                R.string.accessibility_activity_description,
                 AccessibilityActivity::class
             ),
         )
     )
 
     object Markers : ActivityGroup(
-        "Markers",
+        R.string.markers_title,
         listOf(
             Activity(
-                "Advanced Markers",
-                "Adding advanced markers to your map.",
+                R.string.advanced_markers_activity,
+                R.string.advanced_markers_activity_description,
                 AdvancedMarkersActivity::class
             ),
             Activity(
-                "Marker Clustering",
-                "Clustering markers on your map.",
+                R.string.marker_clustering_activity,
+                R.string.marker_clustering_activity_description,
                 MarkerClusteringActivity::class
             ),
             Activity(
-                "Marker Drag Events",
-                "Listening to marker drag events.",
+                R.string.marker_drag_events_activity,
+                R.string.marker_drag_events_activity_description,
                 MarkerDragEventsActivity::class
             ),
             Activity(
-                "Markers Collection",
-                "Adding a collection of markers to your map.",
+                R.string.markers_collection_activity,
+                R.string.markers_collection_activity_description,
                 MarkersCollectionActivity::class
             ),
             Activity(
-                "Syncing Draggable Marker With Data Model",
-                "Keeping a draggable marker in sync with a data model.",
+                R.string.syncing_draggable_marker_with_data_model_activity,
+                R.string.syncing_draggable_marker_with_data_model_activity_description,
                 SyncingDraggableMarkerWithDataModelActivity::class
             ),
             Activity(
-                "Updating No-Drag Marker With Data Model",
-                "Updating a non-draggable marker with a data model.",
+                R.string.updating_no_drag_marker_with_data_model_activity,
+                R.string.updating_no_drag_marker_with_data_model_activity_description,
                 UpdatingNoDragMarkerWithDataModelActivity::class
             ),
             Activity(
-                "Draggable Markers Collection With Polygon",
-                "Dragging a collection of markers that form a polygon.",
+                R.string.draggable_markers_collection_with_polygon_activity,
+                R.string.draggable_markers_collection_with_polygon_activity_description,
                 DraggableMarkersCollectionWithPolygonActivity::class
             ),
         )
     )
 
     object UIIntegration : ActivityGroup(
-        "UI Integration",
+        R.string.ui_integration_title,
         listOf(
             Activity(
-                "Map in Column",
-                "Displaying a map within a column.",
+                R.string.map_in_column_activity,
+                R.string.map_in_column_activity_description,
                 MapInColumnActivity::class
             ),
             Activity(
-                "Maps in LazyColumn",
-                "Displaying multiple maps in a LazyColumn.",
+                R.string.maps_in_lazy_column_activity,
+                R.string.maps_in_lazy_column_activity_description,
                 MapsInLazyColumnActivity::class
             ),
             Activity(
-                "Fragment Demo",
-                "Using the map compose components in a fragment.",
+                R.string.fragment_demo_activity,
+                R.string.fragment_demo_activity_description,
                 FragmentDemoActivity::class
             ),
         )
     )
 
     object Performance : ActivityGroup(
-        "Performance",
+        R.string.performance_title,
         listOf(
             Activity(
-                "Recomposition",
-                "Understanding how recomposition works with maps.",
+                R.string.recomposition_activity,
+                R.string.recomposition_activity_description,
                 RecompositionActivity::class
             ),
         )
@@ -177,8 +179,8 @@ sealed class ActivityGroup(
  * @param kClass The class of the activity to be launched.
  */
 data class Activity(
-    val title: String,
-    val description: String,
+    @StringRes val title: Int,
+    @StringRes val description: Int,
     val kClass: KClass<out ComponentActivity>
 )
 
@@ -215,66 +217,85 @@ fun DemoList(
 
     LazyColumn {
         items(allActivityGroups) { group ->
+            val isExpanded = expandedGroup == group
             Column {
                 // The card representing the group header.
-                Card(
-                    // Highlight the card when it's expanded.
-                    colors = if (expandedGroup == group) {
-                        CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer)
-                    } else {
-                        CardDefaults.cardColors()
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp)
-                        .clickable {
-                            // Toggle the expanded state of the group.
-                            expandedGroup = if (expandedGroup == group) null else group
-                        }
-                ) {
-                    Row(
-                        modifier = Modifier.padding(16.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(
-                            text = group.title,
-                            fontWeight = FontWeight.Bold,
-                            modifier = Modifier.weight(1f)
-                        )
-                        // Show an up or down arrow to indicate the expanded/collapsed state.
-                        Icon(
-                            imageVector = if (expandedGroup == group) {
-                                Icons.Default.KeyboardArrowUp
-                            } else {
-                                Icons.Default.KeyboardArrowDown
-                            },
-                            contentDescription = null
-                        )
-                    }
+                GroupHeaderItem(group, isExpanded) {
+                    expandedGroup = (if (isExpanded) null else group)
                 }
 
                 // Animate the visibility of the activities within the group.
-                AnimatedVisibility(visible = expandedGroup == group) {
+                AnimatedVisibility(visible = isExpanded) {
                     Column(
                         modifier = Modifier.padding(start = 16.dp, end = 16.dp)
                     ) {
                         // Create a card for each activity in the group.
                         group.activities.forEach { activity ->
-                            Card(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(vertical = 4.dp)
-                                    .clickable { onActivityClick(activity.kClass) }
-                            ) {
-                                Column(modifier = Modifier.padding(16.dp)) {
-                                    Text(text = activity.title, fontWeight = FontWeight.Bold)
-                                    Text(text = activity.description)
-                                }
-                            }
+                            DemoActivityItem(onActivityClick, activity)
                         }
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun DemoActivityItem(
+    onActivityClick: (KClass<out ComponentActivity>) -> Unit,
+    activity: Activity
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+            .clickable { onActivityClick(activity.kClass) }
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = stringResource(activity.title), fontWeight = FontWeight.Bold)
+            Text(text = stringResource(activity.description))
+        }
+    }
+}
+
+@Composable
+private fun GroupHeaderItem(
+    group: ActivityGroup,
+    isExpanded: Boolean,
+    onGroupClicked: () -> Unit = {}
+) {
+    Card(
+        // Highlight the card when it's expanded.
+        colors = if (isExpanded) {
+            CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer)
+        } else {
+            CardDefaults.cardColors()
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable {
+                onGroupClicked()
+            }
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(group.title),
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.weight(1f)
+            )
+            // Show an up or down arrow to indicate the expanded/collapsed state.
+            Icon(
+                imageVector = if (isExpanded) {
+                    Icons.Default.KeyboardArrowUp
+                } else {
+                    Icons.Default.KeyboardArrowDown
+                },
+                contentDescription = null
+            )
         }
     }
 }
