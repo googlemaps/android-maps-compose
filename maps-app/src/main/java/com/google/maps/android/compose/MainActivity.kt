e01e2ed675427d5f68c342e90b85ff0358ea889e
dkhawk <107309+dkhawk@users.noreply.github.com>
2025-09-16 16:15:44 -0600

    chore(demo): Add copyright header to Demo.kt and clean up unused import

--- maps-app/src/main/java/com/google/maps/android/compose/MainActivity.kt
+++ maps-app/src/main/java/com/google/maps/android/compose/MainActivity.kt
@@ -25,7 +25,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.CenterAlignedTopAppBar
